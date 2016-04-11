212f82c32f5084d965c9a76f61a7fc165768fb13
Ardeol <new0programmer@gmail.com>
2016-04-10 23:23:38 -0500

    Scaffolding

---
+++ src/edu/tamu/pt/io/generators/Generator.hx
@@ -0,0 +1,5 @@
+package edu.tamu.pt.io.generators;
+
+import edu.tamu.pt.db.IDatabase;
+
+typedef Generator = FileWriter<IDatabase>
---
+++ src/edu/tamu/pt/io/generators/WebPageGenerator.hx
@@ -0,0 +1,21 @@
+package edu.tamu.pt.io.generators;
+
+import sys.io.FileOutput;
+
+import edu.tamu.pt.db.IDatabase;
+
+/** WebPageGenerator Class
+ *  @author  Timothy Foster
+ *  @version A.00
+ *
+ *  **************************************************************************/
+class WebPageGenerator extends Generator {
+
+/**
+ *  @inheritDoc
+ */
+    override private function parse(db:IDatabase, out:FileOutput):Void {
+        
+    }
+    
+}
